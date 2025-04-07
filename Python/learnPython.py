440a2fb1968bf1c2ea1395152dc20739aa983235
Nguyen Huy <nghuy29032004@gmail.com>
2025-04-07 21:03:18 +0700

    solved problem Symmetric Tree

--- Python/learnPython.py
+++ Python/learnPython.py
@@ -246,6 +246,31 @@ class Solution(object):
         # Swap the values of the two incorrect nodes
         if self.first and self.second:
             self.first.val, self.second.val = self.second.val, self.first.val
+
+    def isSymmetric(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not root:
+            return True
+
+        queue = deque()
+        queue.append((root.left, root.right))
+
+        while queue:
+            t1, t2 = queue.popleft()
+            if not t1 and not t2:
+                continue
+            if not t1 or not t2 or t1.val != t2.val:
+                return False
+            
+            queue.append((t1.left, t2.right))
+            queue.append((t1.right, t2.left))
+
+        return True
+    
+    
         
 
 
