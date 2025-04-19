f6b1bad6e0c2c1852d83cdfa2c8db9b89b5f7cc4
Nguyen Huy <nghuy29032004@gmail.com>
2025-04-19 16:22:58 +0700

    code best first search

---
+++ BestFirstSearch.py
@@ -0,0 +1,58 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, name, heuristic):
+        self.name = name
+        self.heuristic = heuristic
+        self.children = []
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+    
+def BestFirstSearch(root : TreeNode, goal):
+    queue = deque()
+    queue.append(root)
+    visited = set()
+    print('Current Node' + '\tList')
+    while(queue):
+        current = queue.popleft()
+        nameNode = current.name
+        
+        if nameNode == goal:
+            print("Goal")
+            return
+        
+        visited.add(current.name)
+        for child in current.children:
+            if child.name not in visited:
+                queue.append(child)
+        queue = deque(sorted(queue, key=lambda node: node.heuristic))
+        print(f'{nameNode}\t\t{[n.name for n in queue]}')
+    print('Not find {goal}')
+# Example Tree
+#        A(6)
+#       /    \
+#    B(4)    C(5)
+#   /   \      \
+# D(7)  E(2)   F(6)
+#          \
+#          G(0)
+
+# Create nodes
+A = TreeNode('A', 6)
+B = TreeNode('B', 4)
+C = TreeNode('C', 5)
+D = TreeNode('D', 7)
+E = TreeNode('E', 2)
+F = TreeNode('F', 6)
+G = TreeNode('G', 0)
+
+# Build tree
+A.add_child(B)
+A.add_child(C)
+B.add_child(D)
+B.add_child(E)
+C.add_child(F)
+E.add_child(G)
+
+BestFirstSearch(A, 'G')
